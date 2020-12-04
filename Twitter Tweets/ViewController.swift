a187204bfbd4b4ac2ead50a128dbce7cab7be8ed
Ivan Raychev <ivanrchv@gmail.com>
2020-12-04 13:35:52 +0200

    first commit

---
+++ Twitter Tweets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Twitter Tweets
+//
+//  Created by Dev on 4.12.20.
+//  Copyright © 2020 Ivan Raychev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
